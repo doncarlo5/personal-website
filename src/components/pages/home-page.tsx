11439ad538429e6c0fb94c1b37918045d7affceb
doncarlos5 <pro.julien.thomas@gmail.com>
2024-02-20 11:26:24 +0100

    Remove unused icons

--- src/components/pages/home-page.tsx
+++ src/components/pages/home-page.tsx
@@ -36,8 +36,6 @@ export function HomePage() {
     })
   }, [api])
 
-  const [isOpen, setIsOpen] = React.useState(false)
-
   return (
     <div>
       <div className="bg-gray-50/80 py-12 lg:py-24">
@@ -415,49 +413,6 @@ function CodepenIcon(props: any) {
   )
 }
 
-function DribbbleIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <circle cx="12" cy="12" r="10" />
-      <path d="M19.13 5.09C15.22 9.14 10 10.44 2.25 10.94" />
-      <path d="M21.75 12.84c-6.62-1.41-12.14 1-16.38 6.32" />
-      <path d="M8.56 2.75c4.37 6 6 9.42 8 17.72" />
-    </svg>
-  )
-}
-
-function WindIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M17.7 7.7a2.5 2.5 0 1 1 1.8 4.3H2" />
-      <path d="M9.6 4.6A2 2 0 1 1 11 8H2" />
-      <path d="M12.6 19.4A2 2 0 1 0 14 16H2" />
-    </svg>
-  )
-}
-
 function IconReact(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="0 0 34 32" fill="currentColor" height="1em" width="1em" {...props}>
@@ -517,14 +472,6 @@ function IconLogoLinkedin(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="0 0 512 512" fill="currentColor" height="1em" width="1em" {...props}>
       <path d="M444.17 32H70.28C49.85 32 32 46.7 32 66.89v374.72C32 461.91 49.85 480 70.28 480h373.78c20.54 0 35.94-18.21 35.94-38.39V66.89C480.12 46.7 464.6 32 444.17 32zm-273.3 373.43h-64.18V205.88h64.18zM141 175.54h-.46c-20.54 0-33.84-15.29-33.84-34.43 0-19.49 13.65-34.42 34.65-34.42s33.85 14.82 34.31 34.42c-.01 19.14-13.31 34.43-34.66 34.43zm264.43 229.89h-64.18V296.32c0-26.14-9.34-44-32.56-44-17.74 0-28.24 12-32.91 23.69-1.75 4.2-2.22 9.92-2.22 15.76v113.66h-64.18V205.88h64.18v27.77c9.34-13.3 23.93-32.44 57.88-32.44 42.13 0 74 27.77 74 87.64z" />
-    </svg>
-  )
-}
-
-function IconGmail(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg viewBox="0 0 24 24" fill="currentColor" height="1em" width="1em" {...props}>
-      <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 010 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z" />
     </svg>
   )
 }
